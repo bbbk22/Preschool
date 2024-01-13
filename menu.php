746e6b080a39a509de03cfc28ae6577c42eaa079
bbbk22 <beltinak15@gmail.com>
2024-01-13 20:24:02 +0100

    update menu

--- menu.php
+++ menu.php
@@ -31,7 +31,7 @@ if (isset($_GET['log_out'])) {
         }
         section{
             padding-top: 90px;
-            background-image: url(backgroundmenu.png);
+            background-image: url(images/backgroundmenu.png);
         }
         .table{
             width: 100%;
