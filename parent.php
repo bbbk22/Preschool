9ddaacb52bab4b7ed4279d7e3f802d719942631b
bbbk22 <beltinak15@gmail.com>
2024-01-13 22:04:53 +0100

    input checkbox

--- parent.php
+++ parent.php
@@ -104,10 +104,14 @@ if (isset($_GET['log_out'])) {
                     <span class="dashboard-box-container-item-span">
                         Breakfast
                     </span>
-                    <span class="dashboard-box-container-item-icon">
-                       <input type="checkbox">
-                        
-                        </span>
+                    <div class="checkbox-wrapper-31">
+                  <input type="checkbox"/>
+                 <svg viewBox="0 0 35.6 35.6">
+                 <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
+                 <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
+                  <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
+                  </svg>
+                  </div>
                 </div>
             </div>
             <div id="schedule" class="dashboard-box-container">
@@ -116,9 +120,14 @@ if (isset($_GET['log_out'])) {
                     <span class="dashboard-box-container-item-span">
                         Lunch
                     </span>
-                    <span class="dashboard-box-container-item-icon">
-                       <input type="checkbox">
-                        </span>
+                    <div class="checkbox-wrapper-31">
+                  <input type="checkbox"/>
+                 <svg viewBox="0 0 35.6 35.6">
+                 <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
+                 <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
+                  <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
+                  </svg>
+                  </div>
                 </div>
             </div>
             <div id="schedule" class="dashboard-box-container">
@@ -127,10 +136,14 @@ if (isset($_GET['log_out'])) {
                     <span class="dashboard-box-container-item-span">
                         Sleep
                     </span>
-                    <span class="dashboard-box-container-item-icon">
-                       <input type="checkbox">
-                    
-                        </span>
+                    <div class="checkbox-wrapper-31">
+                  <input type="checkbox"/>
+                 <svg viewBox="0 0 35.6 35.6">
+                 <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
+                 <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
+                  <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
+                  </svg>
+                  </div>
                 </div>
             </div>
         </div>
